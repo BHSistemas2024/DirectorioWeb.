3c35e16628d76a661a183670456ef8d82c6e5904
BHSistemas2024 <157640736+BHSistemas2024@users.noreply.github.com>
2024-02-20 10:07:57 -0500

    Update tiendascliff.php

--- tiendascliff.php
+++ tiendascliff.php
@@ -1,38 +1,3 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "directorio";
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    if ($conn->connect_error) {
-        die("Error en la conexión: " . $conn->connect_error);
-    }
-
-    $region = $_POST['region'];
-    $storeName = $_POST['storeName'];
-    $administrator = $_POST['administrator'];
-    $cellphone = $_POST['cellphone'];
-    $email = $_POST['email'];
-    $address = $_POST['address'];
-    $provider = $_POST['provider'];
-    $message = $_POST['message'];
-
-    // Reemplazamos caracteres no numéricos en el número de teléfono
-    $cellphone = preg_replace("/[^0-9]/", "", $cellphone);
-
-    $sql = "INSERT INTO tiendascliff (region, storeName, administrator, cellphone, email, address, provider, message) VALUES ('$region', '$storeName', '$administrator', '$cellphone', '$email', '$address', '$provider', '$message')";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "Datos enviados correctamente. ¡Gracias por tu información!";
-    } else {
-        echo "Error al insertar datos en tiendascliff: " . $conn->error;
-    }
-
-    $conn->close();
-}
-?>
 
 
